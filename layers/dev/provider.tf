0cf58f9591082aaa697186d55ddfe3c8cd677c89
Nagarjuna <46648905+nags28@users.noreply.github.com>
2019-07-30 11:42:38 +0530

    Update provider.tf

--- layers/dev/provider.tf
+++ layers/dev/provider.tf
@@ -1,6 +1,6 @@
 provider "aws"{
  region = "${var.region}"
- skip_requesting_account_id  = true
- skip_credentials_validation = true
+ //skip_requesting_account_id  = true
+ //skip_credentials_validation = true
  
 }
