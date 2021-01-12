fc5ae76d1b8ea47fdf9bb25758643c1ffbd77f27
Yukthad <55184068+Yukthad@users.noreply.github.com>
2021-01-12 18:12:34 +0530

    Update ec2.tf

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -6,24 +6,6 @@ resource "aws_instance" "ec2_pro" {
     security_groups= ["${aws_security_group.ALLTRAFFIC.name}"]
     
 }
-resource "aws_security_group" "ALLTRAFFIC" {
-    name="ALLTRAFFIC"
-
-    ingress {
-        from_port = 0
-      to_port = 0
-      protocol = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-
-egress {
-       from_port = 0
-      to_port = 0
-      protocol = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
-
-}
- }
 
  resource "aws_ebs_volume" "volume1" {
     availability_zone = "eu-central-1a"
